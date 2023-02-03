6965ccca8ddd5265c2de885dfcf962cf0a93fa9b
Jesse Carson <jmcarson222@gmail.com>
2023-02-03 15:26:14 -0600

    js 3

--- 3-exp-app.js
+++ 3-exp-app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// setup static and middleware
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/.navbar-app/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "/.navbar-app/index.html"));
+// });
+// ^^^ this added to public folder
 
 app.all("*", (req, res) => {
   res.status(404).send("Resource not found");
---
+++ app.js
@@ -0,0 +1,21 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const { products } = require("./data.js");
+
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+
+  res.json(newProducts);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port: 3000....");
+});
