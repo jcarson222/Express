eaa4dc9d88cb96c690a1a263ced87d889bedc4e0
Jesse Carson <jmcarson222@gmail.com>
2023-02-03 11:42:31 -0600

    http-basic

---
+++ 1-http-basic.js
@@ -0,0 +1,55 @@
+const http = require("http");
+const { readFileSync } = require("fs");
+
+// get all files
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method) (GET)
+  // console.log(req.url) (/, /contact, /about, /purchase...)
+
+  const url = req.url;
+
+  // Home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    // status code : header(s)^^^
+    res.write(homePage);
+    res.end();
+  }
+  // styles.css
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // logo.svg
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  }
+  // browser-app.js
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  }
+  // About page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(3000);
