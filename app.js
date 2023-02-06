951298a269d19d85500eddb181d25d24744400dd
Jesse Carson <jmcarson222@gmail.com>
2023-02-06 11:56:07 -0600

    req.params

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const { application } = require("express");
 const express = require("express");
 const app = express();
-const { products } = require("./data.js");
+const { products } = require("./data");
 
 app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
@@ -14,6 +13,26 @@ app.get("/api/products", (req, res) => {
   });
 
   res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  // keep in mind, ':productID' (also, can be named whatever) will return a string.
+  // will need to convert to a number since our product id's are numbers.
+  //console.log(req.params);
+  // 'params' is whatever you type behind the ':'
+
+  const { productID } = req.params;
+  //console.log({ productID });
+  // ^^^ variable wrapped in {} becasue req is an object
+  // ^^^ 'req.params' = { productID: string}
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+    // ^^^ convert product_id to a number
+  );
+
+  //console.log(singleProduct);
+  return res.json(singleProduct);
 });
 
 app.listen(3000, () => {
