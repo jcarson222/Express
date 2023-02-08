5a1ca402c4849d50e2097ea74ba5ce15d81fc7b7
Jesse Carson <jmcarson222@gmail.com>
2023-02-08 14:00:47 -0600

    router

--- routes/people.js
+++ routes/people.js
@@ -1,75 +1,29 @@
 const express = require("express");
 const router = express.Router();
-let { people } = require("../data");
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people");
 
 // the path for the following routes is only '/' because the "app.use("api/people", people)" in our app.js already defines the path "api/people".
 
-// GET
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+router.get("/", getPeople);
+router.post("/", createPerson);
+router.post("/postman", createPersonPostman);
+router.put("/:id", updatePerson);
+router.delete("/:id", deletePerson);
+// ^^^ I LIKE THIS ONE BETTER ^^^
 
-// POST
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
+// or chain all routes in one line:
+// router
+//   .route("/")
+//   .get(getPeople)
+//   .post(createPerson);
 
-router.post("/postman", (req, res) => {
-  // ^^^ path was "api/people/postman"
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
-
-// PUT
-router.put("/:id", (req, res) => {
-  // ^^^ path was "api/people/:id"
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` });
-  }
-
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-// DELETE
-router.delete("/:id", (req, res) => {
-  // ^^^ path was "api/people/:id"
-  const { id } = req.params;
-  // ^^^ instead of destructuring 'id', you can also just use req.params.id throughout.
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` });
-  }
-
-  const newPeople = people.filter((person) => person.id !== Number(id));
-
-  res.status(200).json({ success: true, data: newPeople });
-});
+// router.route("/postman").post(createPersonPostman);
+// router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
